8dd8f02fb9bd239cf68adc8d442ed33aed504440
wilber <xuwb0410@163.com>
2025-09-14 08:03:14 +0800

    Add files via upload

---
+++ Algorithm/U2_GCD.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# 求两个整数的最大公约数
+
+def Divisor(x, y):
+    reduce = 0
+    if x < y:
+        reduce = x
+    elif x > y:
+        reduce = y
+    else:
+        return x
+    while reduce >= 1:
+        if x % reduce == 0 and y % reduce == 0:
+            return reduce
+        else:
+            reduce -= 1
+
+try:
+    n1 = int(input('请输入第一个整数： '))
+    n2 = int(input('请输入第二个整数： '))
+    if n1 <= 0 or n2 <= 0:
+        print('输入的整数应大于0')
+    else:
+        print('%d 和 %d 的最大公约数是 %d' % (n1, n2, Divisor(n1, n2)))
+except:
+    print('输入数字出错！')
---
+++ Algorithm/U2_PI.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#蒙特卡洛法求pie
+
+from random import random
+
+n = 2000000000 #求pie的精度值，越大越高,但不宜过高(如果你的电脑配置很好，当我没说)
+
+def xy_rang(n):
+    m = 0
+    for i in range(1, n + 1):
+        x = random() * 2 - 1
+        y = random() * 2 - 1
+        if x**2 + y**2 < 1:
+            m += 1
+    return m
+
+c1 = xy_rang(n)
+print('总实验次数是 %d ,计算的圆周率是 %f' % (n, 4 * c1 / n))
